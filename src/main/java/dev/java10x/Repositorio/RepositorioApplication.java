4745a8f5a170d6d478b6a2d1fbe2d3f98c4666ea
Kauã Kyuzo Fujita <271039221+KauaFuijtakkyz@users.noreply.github.com>
2026-03-25 19:08:59 -0300

    first commit

---
+++ src/main/java/dev/java10x/Repositorio/RepositorioApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.Repositorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositorioApplication.class, args);
+	}
+
+}
