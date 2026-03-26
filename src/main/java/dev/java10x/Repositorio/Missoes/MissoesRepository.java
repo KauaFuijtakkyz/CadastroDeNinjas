fc5cbb655d8a82a7f73119ce7f55227061f50977
Kauã Kyuzo Fujita <271039221+KauaFuijtakkyz@users.noreply.github.com>
2026-03-25 22:51:37 -0300

    feat: adiciona Repository

---
+++ src/main/java/dev/java10x/Repositorio/Missoes/MissoesRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.Repositorio.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MIssoesModel, Long> {
+}
