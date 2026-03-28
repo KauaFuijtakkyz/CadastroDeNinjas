25453bcfc103b2f4a360b5e38ef71cf555d867c1
Kauã Kyuzo Fujita <271039221+KauaFuijtakkyz@users.noreply.github.com>
2026-03-28 00:10:39 -0300

    Chore: inclui a versao do DTO pela entidade e mapeia manualmente no ninjaMapper os caminhos entidade x DTO

--- src/main/java/dev/java10x/Repositorio/Ninjas/Controller/NinjaDTO.java
+++ src/main/java/dev/java10x/Repositorio/Ninjas/Controller/NinjaDTO.java
@@ -1,4 +1,19 @@
 package dev.java10x.Repositorio.Ninjas.Controller;
 
+import dev.java10x.Repositorio.Missoes.MIssoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private MIssoesModel missoes;
+    private String rank;
 }
--- src/main/java/dev/java10x/Repositorio/Ninjas/Controller/NinjaMapper.java
+++ src/main/java/dev/java10x/Repositorio/Ninjas/Controller/NinjaMapper.java
@@ -1,4 +1,29 @@
 package dev.java10x.Repositorio.Ninjas.Controller;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NInjaModel map(NinjaDTO ninjaDTO){
+        NInjaModel nInjaModel = new NInjaModel();
+        nInjaModel.setId(ninjaDTO.getId());
+        nInjaModel.setNome(ninjaDTO.getNome());
+        nInjaModel.setEmail(ninjaDTO.getEmail());
+        nInjaModel.setIdade(ninjaDTO.getIdade());
+        nInjaModel.setRank(ninjaDTO.getRank());
+        nInjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return nInjaModel;
+    }
+    public NinjaDTO map(NInjaModel nInjaModel) {
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setNome(nInjaModel.getNome());
+        ninjaDTO.setEmail(nInjaModel.getEmail());
+        ninjaDTO.setIdade(nInjaModel.getIdade());
+        ninjaDTO.setRank(nInjaModel.getRank());
+        ninjaDTO.setMissoes(nInjaModel.getMissoes());
+
+        return ninjaDTO;
+    }
 }
